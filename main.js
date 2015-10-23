49171fbed602e1215b7bd21086213104b90e5815
Jillian Vanderzell <jillianvanderzell@gmail.com>
2015-10-23 14:05:06 -0400

    submenu revisions

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	
+	$(".submenu").hide()
+
+	$(".menu").hover(function(){
+    $(".submenu").show();
+    }, function(){
+    $(".submenu").hide();
+});
+	$(".submenu").hover(function(){
+    $(".submenu").show();
+    }, function(){
+    $(".submenu").hide();
+});
+
+// $(".menu").mouseEnter(function() {
+// 	$(".submenu").show();
+// });
+
+	// $(".menu").mouseEnter(".submenu").show();
+	// $(".menu").mouseLeave(".submenu").hide();
+
+	// $(".menu").hover(function() {
+	// 	$(".submenu").fadeIn(100);
+	// 	$(".submenu").fadeOut(100);
+	// });
+	
+
+	// $("#menu").hover(function(){
+	// 	$(".invisible").toggleClass(".submenu");
+
+});
